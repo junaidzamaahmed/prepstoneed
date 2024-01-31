6dbbf00f4627f050e2fe57c524ce6371e48c1b69
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-01-31 16:30:41 +0600

    added title and favicon

--- src/app/(dashboard)/_components/logo.tsx
+++ src/app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image height={70} width={70} alt="logo" src="/logo.svg" />;
+  return <Image height={70} width={110} alt="logo" src="/logo.png" />;
 };
