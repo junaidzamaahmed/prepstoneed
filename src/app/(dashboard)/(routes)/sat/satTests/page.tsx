26eefcf981930b537c786e01f1439d54482f6c3c
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-03-02 02:32:49 +0600

    added protection

--- src/app/(dashboard)/(routes)/sat/satTests/page.tsx
+++ src/app/(dashboard)/(routes)/sat/satTests/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { UserTable } from "../../../_components/user-table";
+import { toast } from "sonner";
 
 export default async function Home() {
   const { userId } = auth();
@@ -22,6 +23,14 @@ export default async function Home() {
       },
     },
   });
+  const userRole = await db.user.findUnique({
+    where: {
+      externalId: userId,
+    },
+  });
+  if (userRole?.role !== "SPECIAL") {
+    return redirect("/");
+  }
   return (
     <div className="p-4">
       <h3 className="text-3xl font-semibold text-primary">
