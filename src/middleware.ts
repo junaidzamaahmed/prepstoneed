e6c3484b15eec814809b567411509174f591674c
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-05-13 01:09:45 +0600

    made api public v2

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export default authMiddleware({
     "/courses/(.*)",
     "/startCourse/(.*)",
     "/sat/satTests",
-    "/api(.*)",
+    "/api",
   ],
   // ignoredRoutes: ["/api/(.*)"],
   // ignoredRoutes: ["/"],
