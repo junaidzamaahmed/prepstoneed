5043eb5c1a17cc57e6d5c0c2bc420019e0b7dbbb
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-05-12 16:48:02 +0600

    testing purchase page v3

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 export default function HOME() {
+  const { userId } = auth();
+  console.log("userId", userId);
   return (
     <div>
       <div
