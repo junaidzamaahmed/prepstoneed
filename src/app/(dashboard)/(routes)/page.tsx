09e2df403eb8823851e58c899d2c954213104f85
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-05-15 01:48:27 +0600

    modified get-ip-address route in api v11

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -5,7 +5,7 @@ export default async function HOME() {
   const res = await fetch("https://prepstoneedbd.com/api/get-ip-address", {
     method: "GET",
   });
-  console.log(res);
+  console.log(res.json());
   return (
     <div>
       <div
