a7ecb570296d4120c1453c28864cf7ae5b3c4e6b
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-05-15 01:25:40 +0600

    modified get-ip-address route in api v6

--- src/app/api/get-ip-address/route.ts
+++ src/app/api/get-ip-address/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const data = {
     ok: true,
-    ip: req.ip ?? "127.0.0.1",
+    ip: req.ip ? req.ip : "127.0.0.1",
     message: "Hello from the API",
   };
   console.log(data);
