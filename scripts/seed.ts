6bce0bedff17fe67af780565a51edd3766561b15
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-10-29 03:07:47 +0600

    commented out seed.ts to fix blocked scope variable error

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,44 +1,44 @@
-const { PrismaClient } = require("@prisma/client");
-const db = new PrismaClient();
+// const { PrismaClient } = require("@prisma/client");
+// const db = new PrismaClient();
 
-async function main() {
-  try {
-    await db.category.createMany({
-      data: [
-        // {
-        //   name: "Practice",
-        // },
-        // {
-        //   name: "Modules",
-        // },
-        // {
-        //   name: "Full Tests",
-        // },
-        // {
-        //   name: "Adaptive Tests",
-        // },
-        // {
-        //   name: "English",
-        // },
-        // {
-        //   name: "Math",
-        // },
-        // {
-        //   name: "DU",
-        // },
-        {
-          name: "DSAT",
-        },
-        {
-          name: "BUP",
-        },
-      ],
-    });
-    console.log("Success");
-  } catch (error) {
-    console.log("Error seeding the database categories", error);
-  } finally {
-    await db.$disconnect();
-  }
-}
-main();
+// async function main() {
+//   try {
+//     await db.category.createMany({
+//       data: [
+//         // {
+//         //   name: "Practice",
+//         // },
+//         // {
+//         //   name: "Modules",
+//         // },
+//         // {
+//         //   name: "Full Tests",
+//         // },
+//         // {
+//         //   name: "Adaptive Tests",
+//         // },
+//         // {
+//         //   name: "English",
+//         // },
+//         // {
+//         //   name: "Math",
+//         // },
+//         // {
+//         //   name: "DU",
+//         // },
+//         {
+//           name: "DSAT",
+//         },
+//         {
+//           name: "BUP",
+//         },
+//       ],
+//     });
+//     console.log("Success");
+//   } catch (error) {
+//     console.log("Error seeding the database categories", error);
+//   } finally {
+//     await db.$disconnect();
+//   }
+// }
+// main();
